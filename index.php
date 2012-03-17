d20b1d8bfc482c1b84f723a5201face927215fd0
Yancharuk Alexander <alex@itvault.info>
2012-03-17 16:28:17 +0400

    - Добавление библиотек в репозиторий
    [+] lib/Auth.class.inc
    [+] lib/Db.class.inc
    [+] lib/DbException.class.inc
    [+] lib/User.class.inc
    [+] lib/UserTables.class.inc
    [+] index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+//require_once 'includes/db_connect.inc';
+//require_once 'includes/UserTables.class.inc';
+
+define('DEBUG', TRUE);
+
+function __autoload($name) {
+    try {
+        require_once "lib/$name.class.inc";
+    }
+    catch (Exception $e){
+        print $e->getMessage();
+    }
+}
+
+$user_tables = new UserTables();
+
+$user_tables->create();
+
+$user_tables->drop();
+
+
+
+$user = new User;
+var_dump($user);
+$groups = array(8,3,2);
+var_dump($user->hasCredentials($groups));
+
+var_dump(Db::getDebugData());
