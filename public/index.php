392c1a48089fe22204cb259c8a77a4aec4394eae
Yancharuk Alexander <alex@itvault.info>
2012-07-12 07:23:05 +0400

    Templates -> View, tpl -> phtml

--- public/index.php
+++ public/index.php
@@ -18,7 +18,7 @@ use Veles\AutoLoader,
 // окружение: development, production
 define('ENVIRONMENT', 'development');
 define('CONFIG_FILE', realpath('../project/settings.ini'));
-define('TEMPLATE_PATH', realpath('../project/Templates'));
+define('TEMPLATE_PATH', realpath('../project/View'));
 
 set_include_path(implode(PATH_SEPARATOR,
     array(realpath('../project'), realpath('../lib'), get_include_path())
