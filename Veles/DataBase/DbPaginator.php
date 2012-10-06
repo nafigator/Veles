16daeeb9e3e380f8ba0ddd301a44402d4f91cefd
Yancharuk Alexander <alex@itvault.info>
2012-10-06 12:41:06 +0400

    Магия для дополнительных свойств постраничного вывода

--- Veles/DataBase/DbPaginator.php
+++ Veles/DataBase/DbPaginator.php
@@ -18,6 +18,9 @@ namespace Veles\DataBase;
  */
 class DbPaginator
 {
+    // Данные, которые могут быть задействованы при отрисовке
+    protected $data = array();
+
     protected $offset = 1;
     protected $limit  = 5;
     protected $key    = 'id';
@@ -128,5 +131,26 @@ class DbPaginator
     final public function setPrimaryKey($key)
     {
         $this->key = $key;
+    }
+
+    /**
+     * Магия для создания доп. свойств постраничного вывода
+     * @param stirng $name
+     * @param mixed  $value
+     */
+    final public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+    }
+
+    /**
+     * Магия для доступа к свойствам постраничного вывода
+     * @param string $name
+     */
+    final public function __get($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
     }
 }
