1aeb279270746184979df6a74503e7aaf32e1c70
Yancharuk Alexander <alex@itvault.info>
2013-02-16 08:58:35 +0400

    CliColor upgrade

--- Veles/Tools/CliColor.php
+++ Veles/Tools/CliColor.php
@@ -25,6 +25,8 @@ class CliColor
 
     private $style;
 
+    private $string;
+
     private $colors = array(
         'black'  => '0',
         'red'    => '1',
@@ -49,17 +51,17 @@ class CliColor
      * При вызове оборачивает строку esc-последовательностями цвета
      * @param string $string Строка
      */
-    final public function __invoke($string = '')
+    final public function __invoke($string = null)
     {
-        if (empty($string)) {
-            return '';
+        if (null === $string) {
+            if (null === $this->string) {
+                return '';
+            }
+
+            $string =& $this->string;
         }
 
-        $styles = array_keys(array_intersect($this->styles, $this->style));
-        $style  = implode(';', $styles);
-
-        $color = $this->colors[$this->color];
-        return "\033[$style;3{$color}m$string\033[0m";
+        return "\033[{$this->getStyle()};3{$this->getColor()}m$string\033[0m";
     }
 
     /**
@@ -72,7 +74,98 @@ class CliColor
         if (!is_array($style)) {
             throw new Exception('Style parameter must be an array!');
         }
+
+        if (!isset($this->colors[$color])) {
+            throw new Exception('Not valid color!');
+        }
+
         $this->color = $color;
         $this->style = $style;
+    }
+
+    /**
+     * Вывод объекта в виде строки
+     */
+    final public function __toString()
+    {
+        if (null === $this->string) {
+            return;
+        }
+
+        $style = $this->getStyle();
+        $color = $this->getColor();
+
+        return "\033[$style;3{$color}m$this->string\033[0m";
+    }
+
+    /**
+     * Добавление строки
+     * @param string $string Строка для последующего вывода в цвете
+     */
+    final public function setString($string = null)
+    {
+        if (null === $string || !is_string($string)) {
+            throw new Exception('Not valid string!');
+        }
+
+        $this->string = $string;
+    }
+
+    /**
+     * Установка стиля
+     * @param array $style Стиль
+     */
+    final public function setStyle($style = array())
+    {
+        if (!is_array($style)) {
+            throw new Exception('Not valid style!');
+        }
+
+        $styles = array_flip($this->styles);
+
+        foreach ($style as $value) {
+            if (!isset($styles[$value])) {
+                throw new Exception("Not valid style: '$value'!");
+            }
+        }
+
+        $this->style = $style;
+    }
+
+    /**
+     * Установка цвета
+     * @param string $color Цвет
+     */
+    private function setColor($color = null)
+    {
+        if (null === $color || !is_string($style)) {
+            throw new Exception('Not valid color!');
+        }
+
+        if (!isset($this->colors[$color])) {
+            throw new Exception("Not valid color: '$color'!");
+        }
+
+        return $this->colors[$this->color];
+    }
+
+    /**
+     * Получение стиля
+     * @return string
+     */
+    private function getStyle()
+    {
+        $styles = array_keys(array_intersect($this->styles, $this->style));
+
+        return implode(';', $styles);
+    }
+
+    /**
+     * Получение цвета
+     * @return string
+     */
+    private function getColor()
+    {
+        return $this->colors[$this->color];
     }
 }
