38a6ecc5987ef93ec8b1478731c5f2d1c42541b4
Alexander Yancharuk <alex@itvault.info>
2014-12-19 16:41:22 +0300

    ViewAdapterAbstract 100% unit-test coverage

---
+++ Tests/View/Adapters/ViewDriver.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file    ViewDriver.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    2014-12-19 16:23
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Tests\View\Adapters;
+
+/**
+ * Class ViewDriver
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class ViewDriver
+{
+	public function testExec($params)
+	{
+		return is_string($param);
+	}
+}
