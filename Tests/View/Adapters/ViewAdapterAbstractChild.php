7f7116befa96c17de30ff58728125067319c6234
Alexander Yancharuk <alex@itvault.info>
2014-12-19 12:58:00 +0300

    Added new unit-test for ViewAdapterAbstract class

---
+++ Tests/View/Adapters/ViewAdapterAbstractChild.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * @file    ViewAdapterAbstractChild.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    2014-12-19 12:03
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Tests\View\Adapters;
+
+use Veles\View\Adapters\ViewAdapterAbstract;
+
+/**
+ * Class ViewAdapterAbstractChild
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class ViewAdapterAbstractChild extends ViewAdapterAbstract
+{
+	/**
+	 * Driver initialization
+	 */
+	protected function __construct()
+	{
+		$this->setDriver($this);
+	}
+
+	/**
+	 * Output method
+	 *
+	 * @param string $path Path to template
+	 */
+	public function show($path)
+	{
+	}
+
+	/**
+	 * Output View into buffer and save it in variable
+	 *
+	 * @param string $path Path to template
+	 * @return string View content
+	 */
+	public function get($path)
+	{
+	}
+
+	/**
+	 * Check template cache status
+	 *
+	 * @param string $tpl Template file
+	 * @return bool Cache status
+	 */
+	public function isCached($tpl)
+	{
+	}
+
+	public function setCalls($calls)
+	{
+		self::$calls = $calls;
+	}
+
+	public function setInstance($instance)
+	{
+		self::$instance = $instance;
+	}
+
+	public function getCalls()
+	{
+		return self::$calls;
+	}
+
+	public function testCall($param)
+	{
+	}
+}
