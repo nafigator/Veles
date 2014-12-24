234cf93df70f3b8e64d65ad8d6fbe30f159e115d
Alexander Yancharuk <alex@itvault.info>
2014-12-24 08:28:28 +0300

    Added unit-test for RouteStatic class

--- Tests/Routing/RouteStaticTest.php
+++ Tests/Routing/RouteStaticTest.php
@@ -8,37 +8,48 @@ use Veles\Routing\RouteStatic;
  */
 class RouteStaticTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var RouteStatic
-     */
-    protected $object;
+	/**
+	 * @var RouteStatic
+	 */
+	protected $object;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new RouteStatic;
-    }
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new RouteStatic;
+	}
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown()
+	{
+	}
 
-    /**
-     * @covers Veles\Routing\RouteStatic::check
-     * @todo   Implement testCheck().
-     */
-    public function testCheck()
-    {
-        // Remove the following lines when you implement this test.
-        $this->markTestIncomplete(
-            'This test has not been implemented yet.'
-        );
-    }
+	/**
+	 * @covers Veles\Routing\RouteStatic::check
+	 * @dataProvider testCheckProvider
+	 */
+	public function testCheck($pattern, $url, $expected)
+	{
+		$result = $this->object->check($pattern, $url);
+
+		$msg = 'RouteStatic::check() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
+	}
+
+	public function testCheckProvider()
+	{
+		return [
+			['/route/static/', '/route/static/', true],
+			['/route/static/', '/route/', false],
+			['/index.php', '/index.php', true],
+			['/route/static/', '/route/static/index.php', true],
+			['/route/static', '/route/static/index.php', true]
+		];
+	}
 }
