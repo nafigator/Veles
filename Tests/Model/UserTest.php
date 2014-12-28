9650613355a668d741b6b3bcf42720660ba1d0bf
Alexander Yancharuk <alex@itvault.info>
2014-12-28 09:30:43 +0300

    Added ActiveRecord::getById() unit-test

--- Tests/Model/UserTest.php
+++ Tests/Model/UserTest.php
@@ -11,20 +11,20 @@ use Veles\DataBase\DbFilter;
  */
 class UserTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var User
-     */
-    protected $object;
+	/**
+	 * @var User
+	 */
+	protected $object;
 
-    protected static $tbl_name;
+	protected static $tbl_name;
 
-    public static function setUpBeforeClass()
-    {
-        // Create test table
-        $tbl_name = static::$tbl_name = User::TBL_NAME;
+	public static function setUpBeforeClass()
+	{
+		// Create test table
+		$tbl_name = static::$tbl_name = User::TBL_NAME;
 
-        Db::setAdapter(PdoAdapter::instance());
-        Db::query("
+		Db::setAdapter(PdoAdapter::instance());
+		Db::query("
 			CREATE TABLE $tbl_name (
 			  id int(10) unsigned NOT NULL DEFAULT '0',
 			  `group` tinyint(3) unsigned NOT NULL DEFAULT '16',
@@ -40,129 +40,129 @@ class UserTest extends \PHPUnit_Framework_TestCase
 			  KEY email (email)
 			) ENGINE=INNODB DEFAULT CHARSET=utf8
 		");
-        // superpass GlOaUExBSD9HxuEYk2ZFaeDhggU716O
-        Db::query("
+		// superpass GlOaUExBSD9HxuEYk2ZFaeDhggU716O
+		Db::query("
 			INSERT INTO $tbl_name
 				(id, email, hash, short_name, birth_date)
 			VALUES
 				(?, ?, ?, ?, ?)
 		", [
-            1, 'mail@mail.org',
-            '$2a$07$usesomesillystringforeGlOaUExBSD9HxuEYk2ZFaeDhggU716O',
-            'uzzy', '1980-12-12'
-        ], 'issss');
-    }
+			1, 'mail@mail.org',
+			'$2a$07$usesomesillystringforeGlOaUExBSD9HxuEYk2ZFaeDhggU716O',
+			'uzzy', '1980-12-12'
+		], 'issss');
+	}
 
-    public static function tearDownAfterClass()
-    {
-        $table =& static::$tbl_name;
-        Db::query("DROP TABLE $table");
-    }
+	public static function tearDownAfterClass()
+	{
+		$table =& static::$tbl_name;
+		Db::query("DROP TABLE $table");
+	}
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
-    protected function setUp()
-    {
-        $this->object = new User;
-    }
+	/**
+	 * Sets up the fixture, for example, opens a network connection.
+	 * This method is called before a test is executed.
+	 */
+	protected function setUp()
+	{
+		$this->object = new User;
+	}
 
-    /**
-     * Tears down the fixture, for example, closes a network connection.
-     * This method is called after a test is executed.
-     */
-    protected function tearDown()
-    {
-    }
+	/**
+	 * Tears down the fixture, for example, closes a network connection.
+	 * This method is called after a test is executed.
+	 */
+	protected function tearDown()
+	{
+	}
 
-    /**
-     * @covers Veles\Model\User::getId
-     */
-    public function testGetId()
-    {
-        $expected = null;
-        $result = $this->object->getId();
+	/**
+	 * @covers Veles\Model\User::getId
+	 */
+	public function testGetId()
+	{
+		$expected = null;
+		$result = $this->object->getId();
 
-        $msg = 'User::getId() returns wrong result!';
-        $this->assertSame($expected, $result, $msg);
+		$msg = 'User::getId() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
 
-        $this->object->getById(1);
+		$this->object->getById(1);
 
-        $expected = 1;
-        $result = $this->object->getId();
-        $this->assertSame($expected, $result, $msg);
-    }
+		$expected = 1;
+		$result = $this->object->getId();
+		$this->assertSame($expected, $result, $msg);
+	}
 
-    /**
-     * @covers Veles\Model\User::getHash
-     */
-    public function testGetHash()
-    {
-        $expected = null;
-        $result = $this->object->getHash();
+	/**
+	 * @covers Veles\Model\User::getHash
+	 */
+	public function testGetHash()
+	{
+		$expected = null;
+		$result = $this->object->getHash();
 
-        $msg = 'User::getHash() returns wrong result!';
-        $this->assertSame($expected, $result, $msg);
+		$msg = 'User::getHash() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
 
-        $this->object->getById(1);
+		$this->object->getById(1);
 
-        $expected = '$2a$07$usesomesillystringforeGlOaUExBSD9HxuEYk2ZFaeDhggU716O';
-        $result = $this->object->getHash();
-        $this->assertSame($expected, $result, $msg);
-    }
+		$expected = '$2a$07$usesomesillystringforeGlOaUExBSD9HxuEYk2ZFaeDhggU716O';
+		$result = $this->object->getHash();
+		$this->assertSame($expected, $result, $msg);
+	}
 
-    /**
-     * @covers Veles\Model\User::getCookieHash
-     */
-    public function testGetCookieHash()
-    {
-        $expected = null;
-        $result = $this->object->getCookieHash();
+	/**
+	 * @covers Veles\Model\User::getCookieHash
+	 */
+	public function testGetCookieHash()
+	{
+		$expected = null;
+		$result = $this->object->getCookieHash();
 
-        $msg = 'User::getCookieHash() returns wrong result!';
-        $this->assertSame($expected, $result, $msg);
+		$msg = 'User::getCookieHash() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
 
-        $this->object->getById(1);
+		$this->object->getById(1);
 
-        $expected = 'GlOaUExBSD9HxuEYk2ZFaeDhggU716O';
-        $result = $this->object->getCookieHash();
-        $this->assertSame($expected, $result, $msg);
-    }
+		$expected = 'GlOaUExBSD9HxuEYk2ZFaeDhggU716O';
+		$result = $this->object->getCookieHash();
+		$this->assertSame($expected, $result, $msg);
+	}
 
-    /**
-     * @covers Veles\Model\User::getSalt
-     */
-    public function testGetSalt()
-    {
-        $expected = null;
-        $result = $this->object->getSalt();
+	/**
+	 * @covers Veles\Model\User::getSalt
+	 */
+	public function testGetSalt()
+	{
+		$expected = null;
+		$result = $this->object->getSalt();
 
-        $msg = 'User::getSalt() returns wrong result!';
-        $this->assertSame($expected, $result, $msg);
+		$msg = 'User::getSalt() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
 
-        $this->object->getById(1);
+		$this->object->getById(1);
 
-        $expected = '$2a$07$usesomesillystringfor';
-        $result = $this->object->getSalt();
-        $this->assertSame($expected, $result, $msg);
-    }
+		$expected = '$2a$07$usesomesillystringfor';
+		$result = $this->object->getSalt();
+		$this->assertSame($expected, $result, $msg);
+	}
 
-    /**
-     * @covers Veles\Model\User::getGroup
-     */
-    public function testGetGroup()
-    {
-        $expected = 16;
-        $result = $this->object->getGroup();
+	/**
+	 * @covers Veles\Model\User::getGroup
+	 */
+	public function testGetGroup()
+	{
+		$expected = 16;
+		$result = $this->object->getGroup();
 
-        $msg = 'User::getGroup() returns wrong result!';
-        $this->assertSame($expected, $result, $msg);
+		$msg = 'User::getGroup() returns wrong result!';
+		$this->assertSame($expected, $result, $msg);
 
-        $this->object->getById(1);
+		$this->object->getById(1);
 
-        $expected = 16;
-        $result = $this->object->getGroup();
-        $this->assertSame($expected, $result, $msg);
-    }
+		$expected = 16;
+		$result = $this->object->getGroup();
+		$this->assertSame($expected, $result, $msg);
+	}
 }
