a74be5d0492863da3da06751c62301509cefb811
Alexander Yancharuk <alex@itvault.info>
2015-01-06 15:46:35 +0300

    Added md5 validator

---
+++ Validators/Md5Validator.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @file    Md5Validator.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ * @date    2015-01-06 15:30
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Validators;
+
+/**
+ * Class Md5Validator
+ * @author  Yancharuk Alexander <alex@itvault.info>
+ */
+class Md5Validator implements iValidator
+{
+	private $pattern;
+
+	/**
+	 * Constructor
+	 *
+	 * @param bool $case_sensitive Sensitivity flag
+	 */
+	public function __construct($case_sensitive = false)
+	{
+		$this->pattern = $case_sensitive
+			? '/^[a-f\d]{32}$/i'
+			: '/^[a-f\d]{32}$/';
+	}
+
+	/**
+	 * Md5 hash validation
+	 *
+	 * @param string $value Value
+	 * @return bool
+	 */
+	public function check($value)
+	{
+		return (bool) preg_match($this->pattern, $value);
+	}
+}
