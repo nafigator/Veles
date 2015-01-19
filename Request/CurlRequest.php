7731e2facc0c6dbf65f552a54f89fd09fb585d63
Alexander Yancharuk <alex@itvault.info>
2015-01-19 18:47:50 +0300

    Base curl-requests class

---
+++ Request/CurlRequest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @file    CurlRequest.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ * @date    2015-01-20 18:12
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Request;
+
+/**
+ * Class CurlRequest
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ */
+class CurlRequest
+{
+	protected $curl;
+	protected $default_options = [
+		CURLOPT_RETURNTRANSFER => true,		// output to string instead stdout
+		CURLOPT_CONNECTTIMEOUT => 10,		// timeout on connect
+		CURLOPT_TIMEOUT        => 10,		// timeout on request
+	];
+
+	/**
+	 * Creates cURL handler and sets options
+	 *
+	 * @param       $url
+	 * @param array $options
+	 */
+	public function __construct($url, array $options = [])
+	{
+		$this->curl = curl_init($url);
+		curl_setopt_array($this->curl, $options + $this->default_options);
+	}
+
+	/**
+	 * Executes request and by default returns result as string
+	 *
+	 * @return mixed
+	 */
+	public function exec()
+	{
+		return curl_exec($this->curl);
+	}
+}
