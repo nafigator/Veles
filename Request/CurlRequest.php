df1d6c25101d8e3a292c17bfaaf679fa3a9160c3
Alexander Yancharuk <alex@itvault.info>
2015-01-20 21:17:50 +0300

    Added class for post requests

--- Request/CurlRequest.php
+++ Request/CurlRequest.php
@@ -17,12 +17,16 @@ namespace Request;
  */
 class CurlRequest
 {
+	/** @var resource */
 	protected $curl;
+	/** @var array */
 	protected $default_options = [
 		CURLOPT_RETURNTRANSFER => true,		// output to string instead stdout
 		CURLOPT_CONNECTTIMEOUT => 10,		// timeout on connect
 		CURLOPT_TIMEOUT        => 10,		// timeout on request
 	];
+
+	protected $options = [];
 
 	/**
 	 * Creates cURL handler and sets options
@@ -33,8 +37,9 @@ class CurlRequest
 	public function __construct($url, array $options = [])
 	{
 		$this->curl = curl_init();
-		curl_setopt($this->curl, CURLOPT_URL, curl_escape($this->curl, $url));
-		curl_setopt_array($this->curl, $options + $this->default_options);
+
+		$this->setOption(CURLOPT_URL, curl_escape($this->curl, $url));
+		$this->setArrayOptions($options);
 	}
 
 	public function __destruct()
@@ -50,5 +55,62 @@ class CurlRequest
 	public function exec()
 	{
 		return curl_exec($this->curl);
+	}
+
+	/**
+	 * Sets additional header for request
+	 *
+	 * @param array $headers Array of additional headers
+	 *
+	 * @return bool
+	 */
+	public function setRequestHeaders(array $headers)
+	{
+		$this->options[CURLOPT_HTTPHEADER] = $headers;
+
+		return curl_setopt($this->curl, CURLOPT_HTTPHEADER, $headers);
+	}
+
+	/**
+	 * Get headers set for prepared request
+	 *
+	 * @return array
+	 */
+	public function getRequestHeaders()
+	{
+		return isset($this->options[CURLOPT_HTTPHEADER])
+			? $this->options[CURLOPT_HTTPHEADER]
+			: [];
+	}
+
+	/**
+	 * Set curl option
+	 *
+	 * @param $option
+	 * @param $value
+	 *
+	 * @return bool
+	 */
+	public function setOption($option, $value)
+	{
+		$this->options[$option] = $value;
+		return curl_setopt($this->curl, $option, $value);
+	}
+
+	/**
+	 * Sets additional options to curl-request
+	 *
+	 * @param array $options
+	 *
+	 * @return bool
+	 */
+	public function setArrayOptions(array $options)
+	{
+		foreach ($options as $option => $value) {
+			$this->options[$option] = $value;
+		}
+		return curl_setopt_array(
+			$this->curl, $options + $this->default_options
+		);
 	}
 }
---
+++ Request/PostCurlRequest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @file    PostCurlRequest.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ * @date    2015-01-20 20:16
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Request;
+
+/**
+ * Class PostCurlRequest
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ */
+class PostCurlRequest extends CurlRequest
+{
+	/** @var array */
+	protected $default_options = [
+		CURLOPT_RETURNTRANSFER => true,		// output to string instead stdout
+		CURLOPT_CONNECTTIMEOUT => 10,		// timeout on connect
+		CURLOPT_TIMEOUT        => 10,		// timeout on request
+		CURLOPT_POST           => true
+	];
+
+	/**
+	 * Set request data
+	 *
+	 * @param array $data
+	 *
+	 * @return bool
+	 */
+	public function setData(array $data)
+	{
+		return $this->setOption(CURLOPT_POSTFIELDS, $data);
+	}
+}
