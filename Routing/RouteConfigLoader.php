dac844a48218157a5c2a00b4ecd4df68b0dce636
Alexander Yancharuk <alex@itvault.info>
2015-05-24 12:06:28 +0300

    Abstract class for routes config loading

---
+++ Routing/RouteConfigLoader.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file    RouteConfigLoader.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ * @date    2015-05-24 11:57
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Routing;
+
+/**
+ * Class RouteConfigLoader
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ */
+abstract class RouteConfigLoader
+{
+	/** @var  string */
+	protected $path;
+
+	/**
+	 * Load routes data from file
+	 *
+	 * return array
+	 */
+	abstract public function load();
+
+	/**
+	 * Get path to config file
+	 *
+	 * @return string
+	 */
+	public function getPath()
+	{
+		return $this->path;
+	}
+
+	/**
+	 * Set path to config file
+	 *
+	 * @param string $path
+	 */
+	public function setPath($path)
+	{
+		$this->path = $path;
+	}
+}
