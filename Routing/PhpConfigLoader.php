d5bd0b7827c1d0e6fad0f2d6ac67f9b90538a8d6
Alexander Yancharuk <alex@itvault.info>
2015-05-24 12:23:10 +0300

    Routes php config loader

---
+++ Routing/PhpConfigLoader.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @file    PhpConfigLoader.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ * @date    2015-05-24 12:17
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Routing;
+
+/**
+ * Class PhpConfigLoader
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ */
+class PhpConfigLoader extends RouteConfigLoader
+{
+	/**
+	 * Load routes data from file
+	 *
+	 * return array
+	 */
+	public function load()
+	{
+		/** @noinspection PhpIncludeInspection */
+		return include $this->getPath();
+	}
+}
