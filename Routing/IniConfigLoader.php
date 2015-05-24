641a0557dbf7de3a5e70cb9fe433ba59f700a957
Alexander Yancharuk <alex@itvault.info>
2015-05-24 12:17:19 +0300

    Routes ini config loader

---
+++ Routing/IniConfigLoader.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file    IniConfigLoader.php
+ *
+ * PHP version 5.4+
+ *
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ * @date    2015-05-24 12:08
+ * @copyright The BSD 3-Clause License
+ */
+
+namespace Veles\Routing;
+
+/**
+ * Class IniConfigLoader
+ * @author  Yancharuk Alexander <alex at itvault dot info>
+ */
+class IniConfigLoader extends RouteConfigLoader
+{
+	/**
+	 * Load routes data from file
+	 *
+	 * return array
+	 */
+	public function load()
+	{
+		return parse_ini_file($this->getPath(), true);
+	}
+}
