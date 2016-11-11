d52b91e591b1e88be6ee73ea43ece47298bcceae
Yancharuk Alexander <alex@itvault.info>
2016-11-11 13:43:10 +0300

    Type hints update

--- Application/EnvironmentTrait.php
+++ Application/EnvironmentTrait.php
@@ -25,7 +25,7 @@ trait EnvironmentTrait
 	 *
 	 * @param Environment $environment
 	 *
-	 * @return EnvironmentTrait
+	 * @return EnvironmentTrait|Application
 	 */
 	public function setEnvironment(Environment $environment)
 	{
